0580fd20ac41d6069e73c052dcdcf1b3e39550b1
Yogesh Rathore <yogeshrathore251@gmail.com>
2023-12-27 00:06:58 +0530

    Yogesh Rathore: Core Data implemented - Save Data, fetch Data from core data completed.

--- MyAlbum/DetailViewController.swift
+++ MyAlbum/DetailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailViewController: UIViewController {
 
@@ -15,15 +16,116 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var saveButton: UIButton!
     
+    var choosenAlbum = ""
+    var choosenAlbumId = UUID()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        fetchData()
+        
+        let hideKeyboardGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyBoard))
+        self.view.addGestureRecognizer(hideKeyboardGestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageViewTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImagePicker))
+        imageView.addGestureRecognizer(imageViewTapGestureRecognizer)
+    }
+    
+    func fetchData() {
+        if choosenAlbum != "" {
+            // Fetch from Core Data
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Album")
+            let idString = choosenAlbumId.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject] {
+                        
+                        if let name = result.value(forKey: "name") as? String {
+                            nameTextField.text = name
+                        }
+                        
+                        if let location = result.value(forKey: "location") as? String {
+                            locationTextField.text = location
+                        }
+                        
+                        if let date = result.value(forKey: "date") as? String {
+                            dateTextField.text = date
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                    }
+                }
+            } catch {
+                
+            }
+        } else {
+            nameTextField.text = ""
+            locationTextField.text = ""
+            dateTextField.text = ""
+        }
+    }
+    
+    @objc func hideKeyBoard() {
+        self.view.endEditing(true)
+    }
+    
+    @objc func selectImagePicker() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
     }
     
     @IBAction func saveButtonDidTap(_ sender: Any) {
         print("Save")
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newAlbum = NSEntityDescription.insertNewObject(forEntityName: "Album", into: context)
+        
+        // insert Attribute
+        
+        newAlbum.setValue(nameTextField.text!, forKey: "name")
+        newAlbum.setValue(locationTextField.text!, forKey: "location")
+        newAlbum.setValue(dateTextField.text, forKey: "date")
+        newAlbum.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        newAlbum.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("Success")
+        } catch {
+            print("Error")
+        }
+        
+        NotificationCenter.default.post(name: NSNotification.Name("newAlbumData"), object: nil, userInfo: nil)
+        self.navigationController?.popViewController(animated: true)
     }
 
+}
+
+extension DetailViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
 }
--- MyAlbum/HomeViewController.swift
+++ MyAlbum/HomeViewController.swift
@@ -6,29 +6,96 @@
 //
 
 import UIKit
+import CoreData
 
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var albumTableView: UITableView!
+    
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    
+    var selectedAlbum = ""
+    var selectedAlbumId = UUID()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(fetchAlbumData), name: NSNotification.Name(rawValue: "newAlbumData"), object: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(goToDetailView))
+        
+        fetchAlbumData()
     }
-
-
+    
+    @objc func fetchAlbumData() {
+        
+        nameArray.removeAll()
+        idArray.removeAll()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Album")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "name") as? String {
+                        self.nameArray.append(name)
+                    }
+                    
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    
+                    self.albumTableView.reloadData()
+                }
+            }
+        } catch {
+            print("Error during fetch album data")
+        }
+        
+    }
+    
+    @objc func goToDetailView() {
+        performSegue(withIdentifier: "DetailVC", sender: nil)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DetailVC" {
+            let detailVC = segue.destination as! DetailViewController
+            detailVC.choosenAlbum = selectedAlbum
+            detailVC.choosenAlbumId = selectedAlbumId
+        }
+    }
 }
 
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        nameArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Yogesh"
+        cell.textLabel?.text = nameArray[indexPath.row]
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedAlbum = nameArray[indexPath.row]
+        selectedAlbumId = idArray[indexPath.row]
+        performSegue(withIdentifier: "DetailVC", sender: nil)
     }
     
     
